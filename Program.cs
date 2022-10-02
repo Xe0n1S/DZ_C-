f34b7bd1b267dcdc31df8beeaaceb6d24ae00354
Xe0n1S <master.5@mail.ru>
2022-10-03 01:02:45 +0400

    DZ po C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Число {a} больше числа {b}");
+}
+else if (a == b)
+{
+    Console.WriteLine($"Число {a} равно числу {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a} меньше числа {b}");
+}
